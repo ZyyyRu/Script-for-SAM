f2b11a28b49dc8b23717869a17c75df9677449bf
Alexander Gurin <alexandergurin1996@gmail.com>
2016-05-11 01:21:17 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,39 +1,41 @@
-var i, sublist, mainlist, a,str,b;
+var i, sublist, str;
 var count = 17; //Количество символов в вашей персональной ссылке http://steamcommunity.com/id/***/ - количество звездочек
-mainlist = document.getElementsByClassName('badge_row is_link');
-a = document.getElementsByClassName('progress_info_bold');
+var mainlist1 = document.getElementsByClassName('badge_row is_link'); //Список игр, в которых есть карточки
+var mainlist2 = document.getElementsByClassName('progress_info_bold'); //Список игр, в которых должны выпасть карточки
+var time = 7200; //Время игры, после которого выпадают карточки (7200 = 2 часа)
+var fl = true; //true - если две и более игры имеются, в которых нет еще 2 часов время игры; false - в противном случаи
 
-//### Это часть нужна, если есть две и более игры, ни разу неоткрывшиеся ###
-console.log("start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Picker.exe");
-for (i = 0; i < mainlist.length; i++) {
+if (fl) {
+	console.log("start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Picker.exe");
+	for (i = 0; i < mainlist1.length; i++) {
+		try {
+			if (mainlist2[i].innerHTML.length > 19) {
+				sublist = mainlist1[i].getElementsByClassName('badge_row_overlay');
+				str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+count)
+				console.log(str.substring(0,str.length-1));
+			}
+		} catch(e) {}
+	}
+	console.log("ping -n " + 7200 + " 127.0.0.1 > nul");
+	for (i = 0; i < mainlist1.length/2; i++) {
+		try {
+			if (mainlist2[i].innerHTML.length > 19) {
+				console.log("taskkill /f /im SAM.Game.exe");
+				console.log("taskkill /f /im SAM.Game.exe");
+			}
+		} catch(e) {}
+	}
+	if (mainlist1.length%2 == 1)
+		console.log("taskkill /f /im SAM.Game.exe");
+}
+
+for (i = 0; i < mainlist1.length; i++) {
 	try {
-		if (a[i].innerHTML.length > 19) {
-			sublist = mainlist[i].getElementsByClassName('badge_row_overlay');
+		if (mainlist2[i].innerHTML.length > 19) {
+			sublist = mainlist1[i].getElementsByClassName('badge_row_overlay');
 			str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+count)
 			console.log(str.substring(0,str.length-1));
-		}
-	} catch(e) {}
-}
-console.log("ping -n " + 7200 + " 127.0.0.1 > nul");
-for (i = 0; i < mainlist.length/2; i++) {
-	try {
-		if (a[i].innerHTML.length > 19) {
-			console.log("taskkill /f /im SAM.Game.exe");
-			console.log("taskkill /f /im SAM.Game.exe");
-		}
-	} catch(e) {}
-}
-if (mainlist.length%2 == 1)
-	console.log("taskkill /f /im SAM.Game.exe");
-//############################################################################
-
-for (i = 0; i < mainlist.length; i++) {
-	try {
-		if (a[i].innerHTML.length > 19) {
-			sublist = mainlist[i].getElementsByClassName('badge_row_overlay');
-			str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+count)
-			console.log(str.substring(0,str.length-1));
-			console.log("ping -n " + a[i].innerHTML.substring(22)*1800 + " 127.0.0.1 > nul");
+			console.log("ping -n " + mainlist2[i].innerHTML.substring(22)*1800 + " 127.0.0.1 > nul");
 			console.log("taskkill /f /im SAM.Game.exe");
 		}
 	} catch(e) {}
