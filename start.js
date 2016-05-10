29708054c1c577363d31acf1a9591ead07ea84c9
Alexander Gurin <alexandergurin1996@gmail.com>
2016-05-11 00:35:10 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 var i, sublist, mainlist, a,str,b;
+var count = 17; //Количество символов в вашей персональной ссылке http://steamcommunity.com/id/***/ - количество звездочек
 mainlist = document.getElementsByClassName('badge_row is_link');
 a = document.getElementsByClassName('progress_info_bold');
 
@@ -7,7 +8,7 @@ for (i = 0; i < mainlist.length; i++) {
 	try {
 		if (a[i].innerHTML.length > 19) {
 			sublist = mainlist[i].getElementsByClassName('badge_row_overlay');
-			str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+17)
+			str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+count)
 			console.log(str.substring(0,str.length-1));
 		}
 	} catch(e) {}
@@ -28,7 +29,7 @@ for (i = 0; i < mainlist.length; i++) {
 	try {
 		if (a[i].innerHTML.length > 19) {
 			sublist = mainlist[i].getElementsByClassName('badge_row_overlay');
-			str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+17)
+			str = "start C:\\Users\\Alexander\\Documents\\SteamAchievementManager63_hotfix\\SAM.Game.exe " + sublist[0].href.substring(46+count)
 			console.log(str.substring(0,str.length-1));
 			console.log("ping -n " + a[i].innerHTML.substring(22)*1800 + " 127.0.0.1 > nul");
 			console.log("taskkill /f /im SAM.Game.exe");
