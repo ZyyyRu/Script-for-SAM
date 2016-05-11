d48826382d2998fa02e0dd1f567baa291b7477d2
Alexander Gurin <alexandergurin1996@gmail.com>
2016-05-11 22:20:37 +0300

    Вместо переменной count ввел переменную profile

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 /*
-* count - количество символов в вашем id
 * fl - true - если две и более игры имеются, в которых нет еще 2 часов время игры; false - в противном случаи
 * url - путь к вашим exe'шникам, вместо одного "\" использовать два "\\"
 */
-var count = 8;
-var fl = true;
-var url = "C:\\Users\\Alexander\\Documents\\My Workspace\\Script-for-SAM\\";
+var	profile = "http://steamcommunity.com/profiles/76561198184542783",
+	fl = true,
+	url = "C:\\Users\\Alexander\\Documents\\My Workspace\\Script-for-SAM\\";
+/*************************************************************************/
 
 var i, sublist, str, temp1, temp2, kol;
 var mainlist1 = document.getElementsByClassName('badge_row is_link'); //Список игр, в которых есть карточки
 var mainlist2 = document.getElementsByClassName('progress_info_bold'); //Список игр, в которых должны выпасть карточки
-
 console.log("start " + url + "SAM.Picker.exe");
 if (fl) {
 	for (i = kol = 0; i < mainlist1.length; i++) {
@@ -20,7 +19,7 @@ if (fl) {
 				temp2 = temp1[0].innerHTML.trim().substring(6).trim().substring(0,1);
 				if (temp2 == 0 || temp2 == "") {
 					sublist = mainlist1[i].getElementsByClassName('badge_row_overlay');
-					str = "start " + url + "SAM.Game.exe " + sublist[0].href.substring(40+count)
+					str = "start " + url + "SAM.Game.exe " + sublist[0].href.substring(11+profile.length)
 					console.log(str.substring(0,str.length-1));
 					kol++;
 				}
@@ -48,7 +47,7 @@ for (i = 0; i < mainlist1.length; i++) {
 	try {
 		if (mainlist2[i].innerHTML.length > 19) {
 			sublist = mainlist1[i].getElementsByClassName('badge_row_overlay');
-			str = "start " + url + "SAM.Game.exe " + sublist[0].href.substring(40+count)
+			str = "start " + url + "SAM.Game.exe " + sublist[0].href.substring(11+profile.length)
 			console.log(str.substring(0,str.length-1));
 			temp1 = mainlist1[i].getElementsByClassName('badge_title_stats_playtime');
 			temp2 = temp1[0].innerHTML.trim().substring(6).trim().substring(0,1);
--- start.bat
+++
@@ -1,5 +0,0 @@
-start C:\Users\Alexander\Documents\SteamAchievementManager63_hotfix\SAM.Picker.exe
-start C:\Users\Alexander\Documents\SteamAchievementManager63_hotfix\SAM.Game.exe 382240
-ping -n 29800 127.0.0.1 > nul
-taskkill /f /im SAM.Game.exe
-taskkill /f /im SAM.Picker.exe
